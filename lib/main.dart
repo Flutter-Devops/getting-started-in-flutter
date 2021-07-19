10639d5e700b9f7021c4e10328e3a97aa62e12d0
Flutter-Devops <87583510+Flutter-Devops@users.noreply.github.com>
2021-07-20 02:11:58 +0530

    Revert "day5 : Single child scroll view and navigator"

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:just_login/Pages/home_page.dart';
 import 'package:just_login/Pages/login_page.dart';
-import 'package:just_login/Pages/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,9 +25,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
-        MyRoutes.loginRoute: (context) => LoginPage(),
-        MyRoutes.homeRoute: (context) => Homepage(),
-        "/home": (context) => Homepage(),
+        "/home": (context) => LoginPage(),
+        "/login": (context) => Homepage(),
       },
     );
   }
